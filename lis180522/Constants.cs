2bd21c3efe65b5fe39b608662afd4495ff18a087
y <zz.iz.0pnup@gmail.com>
2018-05-22 18:27:59 +0900

    base. download from azure

---
+++ lis180522/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace lis180522
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://lis180522.azurewebsites.net";
+	}
+}
+
